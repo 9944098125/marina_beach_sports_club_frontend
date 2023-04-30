92fd718b3940b7ca59563cf07e77b641b0629333
Srinivas Akella <srinivas.prasad@teknotrait.com>
2023-04-30 14:47:23 +0530

    asdf

--- src/Pages/About/Founders.jsx
+++ src/Pages/About/Founders.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import grid from "../../Assets/Images/grid.png";
 import "./styles.css";
 
 function Founders() {
@@ -11,32 +10,50 @@ function Founders() {
           <h2 className="founders_head">The Founders and Leaders Profiles</h2>
           <div className="grid_row">
             <div className="each_grid">
-              <img src={grid} alt="" />
+              <img
+                src="https://home.barclays/content/dam/home-barclays/images/news/2021/04/HERO-BARCLAYS-INNOVATION-SUPPORTING-BLACK-FOUNDERS-16_9.large.medium_quality.jpg"
+                alt=""
+              />
               <p className="grid_text">Sam Armstrong</p>
               <p className="grid_text_2">Head of Private Equity, Partner</p>
             </div>
             <div className="each_grid">
-              <img src={grid} alt="" />
+              <img
+                src="https://www.stashfin.com/static/media/tushar-photo.a12864fde8cd9a111eea.webp"
+                alt=""
+              />
               <p className="grid_text">Kate Hayward</p>
               <p className="grid_text_2">Partner</p>
             </div>
             <div className="each_grid">
-              <img src={grid} alt="" />
+              <img
+                src="https://media.istockphoto.com/id/1368076322/photo/portrait-of-a-confident-businesswoman-in-an-office.jpg?b=1&s=170667a&w=0&k=20&c=sMc2g5vzwaZG72TNUsiaiB05Uwoa0FcipI_KSGpT8VA="
+                alt=""
+              />
               <p className="grid_text">Brett Scallon</p>
               <p className="grid_text_2">Business Development Manager</p>
             </div>
             <div className="each_grid">
-              <img src={grid} alt="" />
+              <img
+                src="https://home.barclays/content/dam/home-barclays/images/news/2021/04/HERO-BARCLAYS-INNOVATION-SUPPORTING-BLACK-FOUNDERS-16_9.large.medium_quality.jpg"
+                alt=""
+              />
               <p className="grid_text">Sam Armstrong</p>
               <p className="grid_text_2">Head of Private Equity, Partner</p>
             </div>
             <div className="each_grid">
-              <img src={grid} alt="" />
+              <img
+                src="https://www.stashfin.com/static/media/tushar-photo.a12864fde8cd9a111eea.webp"
+                alt=""
+              />
               <p className="grid_text">Kate Hayward</p>
               <p className="grid_text_2">Partner</p>
             </div>
             <div className="each_grid">
-              <img src={grid} alt="" />
+              <img
+                src="https://media.istockphoto.com/id/1368076322/photo/portrait-of-a-confident-businesswoman-in-an-office.jpg?b=1&s=170667a&w=0&k=20&c=sMc2g5vzwaZG72TNUsiaiB05Uwoa0FcipI_KSGpT8VA="
+                alt=""
+              />
               <p className="grid_text">Brett Scallon</p>
               <p className="grid_text_2">Business Development Manager</p>
             </div>
--- src/Pages/Events/index.js
+++ src/Pages/Events/index.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import TopPart from "./TopPart";
 import rows1 from "../../Assets/Images/rows1.png";
-import rows2 from "../../Assets/Images/rows2.png";
-import rows3 from "../../Assets/Images/rows3.png";
 import "./styles.css";
 import Rows from "./Rows";
 
@@ -27,25 +25,25 @@ export default function Events() {
       />
       <Rows
         index={1}
-        image={rows2}
+        image="https://media.istockphoto.com/id/996847394/photo/beach-volleyball.jpg?s=612x612&w=0&k=20&c=-5oyUKlQt1V_uNTUMmVFx6rAq-nV2HHLZ8K1KF1lziw="
         head="FIVB challenge BSNL"
         text="First time in India FIVB was brought - the event was conducted in Chennai Elliot's beach - 32 countries - 32 teams from all aroun the Globe participated and 24 teams were finalized in the selection process - the total prize money was $100,000.00 for Men and Women ($50k for men/$50K for women). Based on the success FIVB alloted us the 2010 event to us"
       />
       <Rows
         index={3}
-        image={rows3}
+        image="https://www.volleytours.com/images/2020/03/02/20190405-sunsation-035.webp"
         head="FIVB challenge SRM"
         text="2010 is a Mega event witnessed by 5000 people, 54 countries involved - 32 teams - 24 in the league - $100k prize money - it was event of extravaganza such as Fashion show, promotional events were conducted in the populated areas of the city for this event and gathered a huge crowd of visitors.  With DJ from Slovakia and music band also involved"
       />
       <Rows
         index={1}
-        image={rows2}
+        image="https://www.reuters.com/resizer/kSbtu7TcpdFwLQWP3wDo-RCPiOs=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/ZCMTODNQNVJLZK4DQQ4WNKWKDY.jpg"
         head="FIVB challenge BSNL"
         text="First time in India FIVB was brought - the event was conducted in Chennai Elliot's beach - 32 countries - 32 teams from all aroun the Globe participated and 24 teams were finalized in the selection process - the total prize money was $100,000.00 for Men and Women ($50k for men/$50K for women). Based on the success FIVB alloted us the 2010 event to us"
       />
       <Rows
         index={3}
-        image={rows3}
+        image="https://i.guim.co.uk/img/media/dbca9f8bd896ad064417153ff9f3aa8d7dfc1319/0_37_3588_2155/master/3588.jpg?width=1200&quality=85&auto=format&fit=max&s=44b9e70f43dcddc88201e856a5d87b8d"
         head="FIVB challenge SRM"
         text="2010 is a Mega event witnessed by 5000 people, 54 countries involved - 32 teams - 24 in the league - $100k prize money - it was event of extravaganza such as Fashion show, promotional events were conducted in the populated areas of the city for this event and gathered a huge crowd of visitors.  With DJ from Slovakia and music band also involved"
       />
