bb6c94ed9d35925f321709122dc5cc87a5404b59
Srinivas Akella <srinivas.prasad@teknotrait.com>
2023-05-09 18:14:36 +0530

    updated code

--- src/Pages/Home/Carousel.jsx
+++ src/Pages/Home/Carousel.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import { FaHandPointer } from "react-icons/fa";
+import { FaHandPointLeft } from "react-icons/fa";
 import { Carousel } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import bannerBg from "../../Assets/Images/bannerBg.png";
-import bbMan from "../../Assets/Images/bbMan.png";
 import miramir from "../../Assets/Images/miramir.png";
 import vBeachCalendar from "../../Assets/Images/vbeachCalendar.png";
-import upWhite from "../../Assets/Images/upWhite.png";
-import upYellow from "../../Assets/Images/upYellow.png";
-import downWhite from "../../Assets/Images/downWhite.png";
-import downYellow from "../../Assets/Images/downYellow.png";
 import "./styles.css";
 
 function CarouselPart() {
@@ -38,20 +33,12 @@ function CarouselPart() {
               </div>
 
               <div className="img_container2">
-                {/* <div className="abstract_container">
-                  <img src={upYellow} alt="" className="yellow_img1" />
-                  <img src={upWhite} alt="" className="yellow_img1" />
-                </div> */}
                 <img src={vBeachCalendar} alt="" className="miramir_img" />
-                <FaHandPointer
+                <FaHandPointLeft
                   fontSize="25px"
                   color="red"
                   className="pointer"
                 />
-                {/* <div className="abstract_container">
-                  <img src={downYellow} alt="" className="yellow_img1a" />
-                  <img src={downWhite} alt="" className="yellow_img1a" />
-                </div> */}
               </div>
             </div>
           </Link>
@@ -75,17 +62,6 @@ function CarouselPart() {
                 </p>
                 <button className="join_btn">Know More →</button>
               </div>
-              {/* <div className="img_container2">
-                <div className="abstract_container">
-                  <img src={upYellow} alt="" className="yellow_img2" />
-                  <img src={upWhite} alt="" className="yellow_img2" />
-                </div>
-                <img src={miramir} alt="" className="miramir_img" />
-                <div className="abstract_container">
-                  <img src={downYellow} alt="" className="yellow_img2a" />
-                  <img src={downWhite} alt="" className="yellow_img2a" />
-                </div>
-              </div> */}
             </div>
           </Link>
         </Carousel.Item>
@@ -113,21 +89,6 @@ function CarouselPart() {
                 </p>
                 <button className="join_btn">Know More →</button>
               </div>
-              {/* <div className="img_container2">
-                <div className="abstract_container">
-                  <img src={upYellow} alt="" className="yellow_img3" />
-                  <img src={upWhite} alt="" className="yellow_img3" />
-                </div>
-                <img
-                  src="https://res.cloudinary.com/dakda5ni3/image/upload/v1683443179/gen1p88gvj10q4g4mwn6.jpg"
-                  alt=""
-                  className="miramir_img3"
-                />
-                <div className="abstract_container">
-                  <img src={downYellow} alt="" className="yellow_img3a" />
-                  <img src={downWhite} alt="" className="yellow_img3a" />
-                </div>
-              </div> */}
             </div>
           </Link>
         </Carousel.Item>
@@ -154,17 +115,6 @@ function CarouselPart() {
                 </p>
                 <button className="join_btn">Know More →</button>
               </div>
-              {/* <div className="img_container2">
-                <div className="abstract_container">
-                  <img src={upYellow} alt="" className="yellow_img3" />
-                  <img src={upWhite} alt="" className="yellow_img3" />
-                </div>
-                <img src={bbMan} alt="" className="miramir_img2" />
-                <div className="abstract_container">
-                  <img src={downYellow} alt="" className="yellow_img3a" />
-                  <img src={downWhite} alt="" className="yellow_img3a" />
-                </div>
-              </div> */}
             </div>
           </Link>
         </Carousel.Item>
